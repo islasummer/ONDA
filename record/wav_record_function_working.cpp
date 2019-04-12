017fb02e7f3841835dfaa43c5135fe135fb299be
Dan-escu <denewick@gmail.com>
2019-04-12 15:38:40 +0100

    Updated buffer code

--- record/wav_record_function_working.cpp
+++ record/wav_record_function_working.cpp
@@ -1,4 +1,7 @@
 #include "wav_header_working.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
 
 
 
@@ -12,7 +15,26 @@ uint16_t waveRecorder::bits_per_sample = 16;
 uint32_t waveRecorder::bytes_per_second = sample_rate * number_of_channels * bits_per_sample / 8;
 uint16_t waveRecorder::bytes_per_frame = number_of_channels * bits_per_sample / 8;
 
+int binaryToDecimal(int n)
+{
+    int num = n;
+    int dec_value = 0;
 
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+
+    int temp = num;
+    while (temp) {
+        int last_digit = temp % 10;
+        temp = temp / 10;
+
+        dec_value += last_digit * base;
+
+        base = base * 2;
+    }
+
+    return dec_value;
+}
 
 int waveRecorder::recordWAV(){
     //test initializing duration
@@ -167,6 +189,9 @@ int waveRecorder::recordWAV(){
             write(filedesc, buffer, size);
         }
     }
+
+    cout << binaryToDecimal(*buffer) << endl;
+
 
     close(filedesc);
 
