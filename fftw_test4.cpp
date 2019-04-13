e2776e7c62642c07f5cd51cb2ac5aa4de8d4c32a
Dan-escu <denewick@gmail.com>
2019-04-13 14:02:01 +0100

    Added line to throw exception

--- fftw_test4.cpp
+++ fftw_test4.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <iomanip>
 #include <cstdlib>
-#include <record/wav_header_working.h>
 #include "fftw3.h"
 #include "pcmread.h"
 #include "pitchdetectclass.h"
@@ -16,14 +15,15 @@ using namespace std;
 int main(int argc, char *argv[])
  {
 
+    //while(true) {
 
-  fftw_complex *in = NULL, *out = NULL;    // Define the input and output array pointers
-  fftw_plan plan;                             // Create a plan for the fft
-  int i = 0;
+        fftw_complex *in = NULL, *out = NULL;    // Define the input and output array pointers
+        fftw_plan plan;                             // Create a plan for the fft
+        int i = 0;
 
 // Allocate the input and output arrays
-  in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-  out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+        in = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * N);
+        out = (fftw_complex *) fftw_malloc(sizeof(fftw_complex) * N);
 
 
 // Load audio data from PCM file
@@ -33,88 +33,94 @@ int main(int argc, char *argv[])
   pcm.read("/home/dan/Downloads/test-note-2.pcm");
 */
 
-  //int i = 0;
+        //int i = 0;
 
-    int err;
-    waveRecorder recorder = waveRecorder();
-
-    double vals[N];
-
-    for(int j = 0; j < 1; ++j) {
-        auto res = recorder.recordWAV();
-
-        for (int j = 0; j < res.first / 2; j++) {
-            short x = *(short *)(res.second + j * 2);
-            //std::cout << x << std::endl;
-            vals[j] = x;
-            //std::cout << vals[j] << std::endl;
+        int err;
+        waveRecorder recorder;
+        try {
+            recorder = waveRecorder();
+        } catch(...) {
+            std::cout << "Exception thrown!\n";
+            return -1;
         }
-        free(res.second);
-    }
+
+        double vals[N];
+
+        for (int j = 0; j < 1; ++j) {
+            auto res = recorder.recordWAV();
+
+            for (int j = 0; j < res.first / 2; j++) {
+                short x = *(short *) (res.second + j * 2);
+                //std::cout << x << std::endl;
+                vals[j] = x;
+                //std::cout << vals[j] << std::endl;
+            }
+            free(res.second);
+        }
 
 
-  for (i = 0; i < N ; i ++)
-  {
-    in[i][0] = vals[i];
-    in[i][1] = 0;
-  }
+        for (i = 0; i < N; i++) {
+            in[i][0] = vals[i];
+            in[i][1] = 0;
+        }
 
 
 // Print input values into .txt file
- ofstream data;
- data.open ("/home/dan/fftwtest.txt");
-  for (i = 0; i < N; i++) {
-    data << left << setw(5) << i << setw(15) << in[i][0] << setw(10) << in[i][1] << endl;
-  }
+        ofstream data;
+        data.open("/home/dan/fftwtest.txt");
+        for (i = 0; i < N; i++) {
+            data << left << setw(5) << i << setw(15) << in[i][0] << setw(10) << in[i][1] << endl;
+        }
 
- // Apply Hann window to the data
+        // Apply Hann window to the data
 //
-  for (int i = 0; i < N; i++)
-  {
-    double multiplier = 0.5 * (1 - cos(2 * M_PI * i/N));
-    in[i][0] = multiplier * in[i][0];
-  }
+        for (int i = 0; i < N; i++) {
+            double multiplier = 0.5 * (1 - cos(2 * M_PI * i / N));
+            in[i][0] = multiplier * in[i][0];
+        }
 //
 
-  // Complex 1d fft function
-  plan = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-  // Real input to complex output function below
-  //p = fftw_plan_dft_r2c_1d(N, in[0], reinterpret_cast<fftw_complex*>(&out[0]), FFTW_ESTIMATE);
+        // Complex 1d fft function
+        plan = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+        // Real input to complex output function below
+        //p = fftw_plan_dft_r2c_1d(N, in[0], reinterpret_cast<fftw_complex*>(&out[0]), FFTW_ESTIMATE);
 
-  // Execute the fft
-  fftw_execute(plan);
+        // Execute the fft
+        fftw_execute(plan);
 
-  // Calculate complex output vector for plotting
-  double y[i];
-  double maxval = 0;
-  double freq = 0;
+        // Calculate complex output vector for plotting
+        double y[i];
+        //double maxval = 0;
+        //double freq = 0;
 
-  for (i = 0; i < N; i++)
-  {
-    double a = out[i][0];
-    double b = out[i][1];
-    y[i] = sqrt(a*a+b*b) / N ;
-  }
+        for (i = 0; i < N; i++) {
+            double a = out[i][0];
+            double b = out[i][1];
+            y[i] = sqrt(a * a + b * b) / N;
+        }
 
- // Retrieve maximum value and estimate fundamental frequency
+        // Retrieve maximum value and estimate fundamental frequency
 
-   pitchdetect pitch;
-   pitch.detect(y);
+        pitchdetect pitch;
+        pitch.detect(y);
 
-  // Print output values into .txt file
-  ofstream data2;
-  data2.open ("/home/dan/fftwtest2.txt");
-    for (i = 0; i < N; i++) {
-      data2 << left << setw(5) << i << setw(15) << abs(y[i]) << endl;
-      }
+        cout << "Maximum value is: " << pitch.maxval << endl << "Fundamental frequency is: " << pitch.freqhz << endl;
+
+        // Print output values into .txt file
+        ofstream data2;
+        data2.open("/home/dan/fftwtest2.txt");
+        for (i = 0; i < N; i++) {
+            data2 << left << setw(5) << i << setw(15) << abs(y[i]) << endl;
+        }
 
 
-  // Deallocate plan
-  fftw_destroy_plan(plan);
+        // Deallocate plan
+        fftw_destroy_plan(plan);
 
-  // Deallocate arrays
-  fftw_free(in);
-  fftw_free(out);
+        // Deallocate arrays
+        fftw_free(in);
+        fftw_free(out);
+   // }
 
-  return 0;
+         return 0;
 }
